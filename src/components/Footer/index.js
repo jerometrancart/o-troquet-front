f2a11749c6f9a12fcc7a56da986a0d3946275b45
Thomas-Radulesco <tom.radulesco@gmail.com>
2020-06-17 14:39:09 +0200

    ok admin button in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-const Footer = () => (
+const Footer = ({ loggedAsAdmin }) => (
   <footer>Mentions légales + Qui sommes-nous?
+
+    {loggedAsAdmin && (
+      <Button color="blue" className="center aligned">Administration
+      </Button>
+    )}
+
   </footer>
 );
+
+Footer.propTypes = {
+  loggedAsAdmin: PropTypes.bool,
+};
+Footer.defaultProps = {
+  loggedAsAdmin: true,
+};
 
 export default Footer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,17 @@
-import { combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import {reducer as BurgerMenu } from 'redux-burger-menu';
 import counter from './counter';
+
 
 export default combineReducers({
   counter,
 });
+
+
+/* const reducers = {
+  // Your other reducers go here
+  BurgerMenu // Must be mounted at 'burgerMenu'
+};
+
+const store = createStore(reducer);
+*/
