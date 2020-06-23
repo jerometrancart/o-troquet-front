6ad1d6a3334396ea6764b19cb84fa9449d87daaa
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-06-23 22:11:33 +0200

    scss added with some rules

--- src/components/Nav/SideBar.js
+++ src/components/Nav/SideBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Dropdown, Menu, Sidebar } from 'semantic-ui-react';
-
+import './style.scss';
 export default class MenuExampleVerticalDropdown extends Component {
   state = { activeItem: 'account' }
 
